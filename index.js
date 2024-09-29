9810d51600293fba8f664df124ed3c1b549c7cc4
AdrianBurke1 <adrianburke902@gmail.com>
2024-09-29 19:21:35 -0400

    update

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
+Band.hasMany(Musicians, {
+    foreignKey: 'bandId',
+    as: 'musicians',
+});
+
+Musician.belongsTo(Band, {
+    foreignKey: 'bandId',
+    as: 'band'
+});
+
+Band.belongsToMany(Songs, { through: 'BandSongs', as: 'songs'});
+Song.belongsToMany(Band, {through: 'BandSongs', as: 'bands'});
 
 
 
